7fd7fbfec52f85dffa00accd2e8cdff251574f1a
Prashant Lakhlani <>
2023-12-22 17:22:17 +0530

    Source Code - Part 2: How to implement Microsoft Entra ID Authentication in Blazor Server Web App in .NET 8

---
+++ BlazorAppPart2/Program.cs
@@ -0,0 +1,42 @@
+using BlazorAppPart2.Components;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration);
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddControllersWithViews(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+}).AddMicrosoftIdentityUI();
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+app.MapControllers();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Blog source code
 This repository hosts source code of the blogs that we post at https://www.faciletechnolab.com/blog
 
-## Blazor Dashboard Example
-
+## Blazor Server Web App Development using .NET 8
+- [Source Code - Part 2: How to implement Microsoft Entra ID Authentication in Blazor Server Web App in .NET 8 ](/facile-technolab-pvt-ltd/blog-source-code/tree/master/BlazorAppPart2)
 
